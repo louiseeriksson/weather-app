f2be825ede96fdb9af3dac809e059de0b495fc10
Louise Eriksson <epost.louise@gmail.com>
2020-07-07 16:00:16 +0200

    one more fetch for the forecast

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React, { useState, useEffect } from 'react'
 import { Header } from 'components/Header'
 import { City } from 'components/City'
 import { TodaysWeather } from 'components/TodaysWeather'
+import { Forecast } from 'components/Forecast'
 import { Footer } from 'components/Footer'
 
 export const App = () => {
 
   const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=b09042e161870e44988114035ff61156'
+  const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast?q=Stockholm,Sweden&units=metric&APPID=b09042e161870e44988114035ff61156'
   const [weather, setWeather] = useState({})
+  const [forecast, setForecast] = useState({})
 
   useEffect(() => {
     fetch(WEATHER_URL)
@@ -20,11 +23,22 @@ export const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    fetch(FORECAST_URL)
+      .then((res) => {
+        return res.json()
+      })
+      .then(JSON => {
+        setForecast(JSON)
+      })
+  }, [])
+
   return (
     <div>
       <Header />
       <City weather={weather} />
       <TodaysWeather weather={weather} />
+      <Forecast forecast={forecast} />
       <Footer />
     </div>
   )
---
+++ src/components/Forecast.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Forecast = ({ forecast }) => {
+  return (
+    <div>
+      <h3>forecast:</h3>
+      <h3>
+        {forecast.list &&
+          `${new Date(forecast.list.filter(item => item.dt_txt.includes('12:00'))[0].dt_txt.replace(" ", "T")).toLocaleDateString('en-US', { weekday: "short" })} : ${Math.round(forecast.list.filter(item => item.dt_txt.includes('12:00'))[0].main.temp)}º C, ${forecast.list.filter(item => item.dt_txt.includes('12:00'))[0].weather[0].description}`}
+      </h3>
+      <h3>
+        {forecast.list &&
+          `${new Date(forecast.list.filter(item => item.dt_txt.includes('12:00'))[1].dt_txt.replace(" ", "T")).toLocaleDateString('en-US', { weekday: "short" })} : ${Math.round(forecast.list.filter(item => item.dt_txt.includes('12:00'))[1].main.temp)}º C, ${forecast.list.filter(item => item.dt_txt.includes('12:00'))[1].weather[0].description}`}
+      </h3>
+      <h3>
+        {forecast.list &&
+          `${new Date(forecast.list.filter(item => item.dt_txt.includes('12:00'))[2].dt_txt.replace(" ", "T")).toLocaleDateString('en-US', { weekday: "short" })} : ${Math.round(forecast.list.filter(item => item.dt_txt.includes('12:00'))[2].main.temp)}º C, ${forecast.list.filter(item => item.dt_txt.includes('12:00'))[2].weather[0].description}`}
+      </h3>
+      <h3>
+        {forecast.list &&
+          `${new Date(forecast.list.filter(item => item.dt_txt.includes('12:00'))[3].dt_txt.replace(" ", "T")).toLocaleDateString('en-US', { weekday: "short" })} : ${Math.round(forecast.list.filter(item => item.dt_txt.includes('12:00'))[3].main.temp)}º C, ${forecast.list.filter(item => item.dt_txt.includes('12:00'))[3].weather[0].description}`}
+      </h3>
+      <h3>
+        {forecast.list &&
+          `${new Date(forecast.list.filter(item => item.dt_txt.includes('12:00'))[4].dt_txt.replace(" ", "T")).toLocaleDateString('en-US', { weekday: "short" })} : ${Math.round(forecast.list.filter(item => item.dt_txt.includes('12:00'))[4].main.temp)}º C, ${forecast.list.filter(item => item.dt_txt.includes('12:00'))[4].weather[0].description}`}
+      </h3>
+
+
+    </div>
+  )
+}
